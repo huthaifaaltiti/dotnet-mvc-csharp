d25ffac897c45d3ac5ea556c49b2eacb5cb54f9a
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2023-08-30 15:54:07 +0300

    first commit

---
+++ GetEmpStatMVCWebApp/GetEmpStatMVCWebApp/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace GetEmpStatMVCWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string apiUrl = "http://localhost/WebAPI/Api/users/";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
+            request.Method = WebRequestMethods.Http.Get;
+            request.Accept = "application/json";
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string responseData = reader.ReadToEnd();
+
+                List<User> users = JsonConvert.DeserializeObject<List<User>>(responseData);
+
+                return View(users);
+            }
+
+           
+        }
+
+        public ActionResult GetAllUsers()
+        {
+            string apiUrl = "http://localhost/WebAPI/Api/users/";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
+            request.Method = WebRequestMethods.Http.Get;
+            request.Accept = "application/json";
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string responseData = reader.ReadToEnd();
+
+                List<User> users = JsonConvert.DeserializeObject<List<User>>(responseData);
+
+                return Json(users, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult GetUser(int id)
+        {
+            string apiUrl = $"http://localhost/WebAPI/Api/users/{id}";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
+            request.Method = WebRequestMethods.Http.Get;
+            request.Accept = "application/json";
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string responseData = reader.ReadToEnd();
+
+                User user = JsonConvert.DeserializeObject<User>(responseData);
+
+                return Json(user, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ GetEmpStatMVCWebApp/GetEmpStatMVCWebApp/Models/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetEmpStatMVCWebApp.Controllers
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+        public int NationalNumber { get; set; }
+    }
+}
