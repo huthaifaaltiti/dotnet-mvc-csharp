5e8443d36deae3ebc791103471c009f22c57af55
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2023-08-31 11:43:25 +0300

    editing user class to view avg, largest salaries, and salary status

--- GetEmpStatMVCWebApp/GetEmpStatMVCWebApp/Models/UserController.cs
+++ GetEmpStatMVCWebApp/GetEmpStatMVCWebApp/Models/UserController.cs
@@ -9,7 +9,13 @@ namespace GetEmpStatMVCWebApp.Controllers
     public class User
     {
         public string Username { get; set; }
-        public string Email { get; set; }
+        //public string Email { get; set; }
         public int NationalNumber { get; set; }
+
+        public decimal AvgSalary { get; set; }
+
+        public string UserSalaryStatus { get; set; }
+
+        public decimal LargestSalary { get; set; }
     }
 }
